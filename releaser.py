9f8cb83433f96a831b99fab793de0cabc12aedf0
M0WUT <dan.m0wut@gmail.com>
2023-11-06 21:43:34 +0000

    .

--- releaser.py
+++ releaser.py
@@ -1,1 +1,14 @@
-print("HELLO WORLD :)")
+import sys
+import pathlib
+import subprocess
+
+
+def main(project_folder: str, release_folder: str):
+    print(f"Releasing project in {project_folder} into {release_folder}")
+
+
+if __name__ == "__main__":
+    main(
+        project_folder=pathlib.PurePath(sys.argv[1]),
+        release_folder=pathlib.PurePath(sys.argv[2]),
+    )
