62762f822e9007644acb3ad127e35160a0455d08
M0WUT <dan.m0wut@gmail.com>
2023-11-11 12:25:06 +0000

    .

--- releaser.py
+++ releaser.py
@@ -86,12 +86,28 @@ def generate_webpage(
     )
     result.check_returncode()
 
+def create_kicad_config():
+    config_path = pathlib.Path(".config") / "kicad" / "7.0"
+    config_path.mkdir(parents=True, exist_ok=True)
+    commands = [
+        "cp",
+        "-r",
+        "kicad_releaser/kicad_settings"   
+        config_path.absolute() 
+    ]
+
+    result = subprocess.run(
+        commands,
+        # capture_output=True,
+    )
+    result.check_returncode()
+
 
 def main(project_folder: pathlib.Path, release_folder: pathlib.Path):
     print(
         f"Releasing project in {project_folder.absolute()} into {release_folder.absolute()}"
     )
-
+    create_kicad_config()
     project_name = discover_kicad_projects(project_folder)
     # generate_schematic_pdf(
     #     project_folder / f"{project_name}.kicad_sch", release_folder / "schematic.pdf"
