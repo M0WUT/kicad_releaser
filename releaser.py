a5f3921e39e6d675f4faee20010a6948f46d3b08
M0WUT <dan.m0wut@gmail.com>
2024-05-01 14:38:02 +0100

    typo

--- releaser.py
+++ releaser.py
@@ -241,12 +241,12 @@ def main(
         create_ibom(x, release_folder)
         if bom_checker:
             bom_checker.run(
-                x.with_suffix(".kicad_sch").absolute(), pathlib.Path(release_folder / f"{x.stem}-bom.md"),
-                mouser_basket=pathlib.Path(release_folder / f"{x.stem}-mouser-bom.csv"),
-                farnell_basket=pathlib.Path(release_folder / f"{x.stem}-farnell-bom.csv"),
+                x.with_suffix(".kicad_sch").absolute(), release_folder / f"{x.stem}-bom.md",
+                mouser_basket=release_folder / f"{x.stem}-mouser-bom.csv",
+                farnell_basket=release_folder / f"{x.stem}-farnell-bom.csv"
             )
             comment = markdown2.markdown_path(
-                release_folder / pathlib.Path(f"{x.stem}-bom.md").absolute(),
+                (release_folder / f"{x.stem}-bom.md").absolute(),
                 extras=["fenced-code-blocks", "tables"],
             )
         else:
